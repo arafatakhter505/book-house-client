a314f222827bddd4a1659c5d4619f82b838f496d
arafatakhter505 <arafatakhterforhad@gmail.com>
2022-12-08 18:34:40 +0600

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
+  const menuItems = [
+    { name: "home", link: "/" },
+    { name: "about", link: "/" },
+    { name: "services", link: "/" },
+    { name: "contact", link: "/" },
+  ];
+
+  const menu = (
+    <>
+      {menuItems.map((item) => (
+        <li key={item.link}>
+          <Link to={item.link}>{item.name}</Link>
+        </li>
+      ))}
+    </>
+  );
+
   return (
     <nav className="bg-base-100 z-50 sticky top-0">
-      <div className="navbar container mx-auto">
+      <div className="navbar container mx-auto py-4">
         <div className="navbar-start">
-          {/* <div className="dropdown">
+          <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -27,41 +45,34 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menu}
             </ul>
-          </div> */}
-          <Link to={"/"} className="text-xl font-semibold">
-            Book House
+          </div>
+          <Link to={"/"} className="lg:text-3xl text-xl font-bold">
+            Book <span className="text-primary">House</span>
           </Link>
         </div>
-        <div className="navbar-center hidden lg:flex">
+        <div className="navbar-center w-1/2 hidden md:flex">
+          <div className="form-control w-full">
+            <div className="input-group">
+              <select className="select select-bordered">
+                <option>Book</option>
+                <option>Writer</option>
+                <option>Publisher</option>
+                <option>Category</option>
+              </select>
+              <input
+                type="text"
+                placeholder="Search…"
+                className="input input-bordered w-full"
+              />
+              <button className="btn btn-square btn-primary text-white text-3xl">
+                <AiOutlineSearch />
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
             <li>
               <a>Item 1</a>
@@ -92,9 +103,35 @@ const Navbar = () => {
               <a>Item 3</a>
             </li>
           </ul>
-        </div>
+        </div> */}
         <div className="navbar-end">
-          <a className="btn">Get started</a>
+          <button className="btn btn-ghost text-4xl">
+            <AiOutlineShoppingCart />
+          </button>
+          <button className="btn btn-primary btn-outline lg:btn btn-sm normal-case ml-3">
+            Sign In
+          </button>
+        </div>
+      </div>
+      <div className="hidden lg:block text-xl container mx-auto">
+        <ul className="menu menu-horizontal p-0">{menu}</ul>
+      </div>
+      <div className="form-control container mx-auto md:hidden px-4">
+        <div className="input-group">
+          <select className="select select-bordered">
+            <option>Book</option>
+            <option>Writer</option>
+            <option>Publisher</option>
+            <option>Category</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Search…"
+            className="input input-bordered w-full"
+          />
+          <button className="btn btn-square btn-primary text-white text-3xl">
+            <AiOutlineSearch />
+          </button>
         </div>
       </div>
     </nav>
